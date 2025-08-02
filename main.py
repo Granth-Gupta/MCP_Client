776464d6787056251ddc52a95152dea53ac80c60
GRANTH GUPTA <102872537+Granth-Gupta@users.noreply.github.com>
2025-08-02 22:39:56 +0530

    .

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ app.add_middleware(
         "https://your-frontend-domain.com",  # TODO: Replace with actual frontend domain(s)
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:5174"
     ],
     allow_credentials=True,
     allow_methods=["*"],
